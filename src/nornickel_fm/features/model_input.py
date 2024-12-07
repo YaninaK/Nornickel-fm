722590931f2f595179e02565192c22192ddf062e
YaninaK <kutovaiayp@yahoo.com>
2024-12-07 20:50:09 +0300

    Update model_input.py

--- src/nornickel_fm/features/model_input.py
+++ src/nornickel_fm/features/model_input.py
@@ -29,3 +29,41 @@ def make_dataset_ore(
     y_fm_1 = df.loc[fm_1_valid_ind, config["fm_1_target_cols"]]
 
     return X_fm1, y_fm_1
+
+
+def make_dataset_cu(
+    df: pd.DataFrame, config: Optional[dict] = None
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Generates dataset for modeling flotation process of the 2nd and the 3d floating machine
+
+    """
+    if config is None:
+        config = CONFIG
+
+    fm_2_valid_ind = df[
+        df[config["fm_2_target_cols"]].notnull().sum(axis=1)
+        == len(config["fm_2_target_cols"])
+    ].index
+    fm_3_valid_ind = df[
+        df[config["fm_3_target_cols"]].notnull().sum(axis=1)
+        == len(config["fm_3_target_cols"])
+    ].index
+
+    X_fm_2 = df.loc[fm_2_valid_ind, config["fm_2_feature_cols"]]
+    X_fm_2["fm"] = 0
+    y_fm_2 = df.loc[fm_2_valid_ind, config["fm_2_target_cols"]]
+
+    X_fm_3 = df.loc[fm_3_valid_ind, config["fm_3_feature_cols"]]
+    X_fm_3["fm"] = 1
+    y_fm_3 = df.loc[fm_3_valid_ind, config["fm_3_target_cols"]]
+
+    X_fm_2.columns = config["fm_2_3_feature_cols"]
+    X_fm_3.columns = config["fm_2_3_feature_cols"]
+    y_fm_2.columns = config["fm_2_3_target_cols"]
+    y_fm_3.columns = config["fm_2_3_target_cols"]
+
+    X_fm_2_3 = pd.concat([X_fm_2, X_fm_3], axis=0)
+    y_fm_2_3 = pd.concat([y_fm_2, y_fm_3], axis=0)
+
+    return X_fm_2_3, y_fm_2_3
