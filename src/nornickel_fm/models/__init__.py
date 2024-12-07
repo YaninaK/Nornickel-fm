d768dc53c5a2238aea6d39262db179f128f8a8eb
YaninaK <kutovaiayp@yahoo.com>
2024-12-07 23:44:04 +0300

    Update __init__.py

--- src/nornickel_fm/models/__init__.py
+++ src/nornickel_fm/models/__init__.py
@@ -4,5 +4,8 @@ MODEL_CONFIG = {
     "fm_1_n_output_units": 4,
     "fm_2_3_units_max": 32,
     "fm_2_3_units_min": 16,
-    "fm_2_3_n_output_units": 4,
+    "fm_2_3_n_output_units": 2,
+    "fm_4_5_6_units_max": 32,
+    "fm_4_5_6_units_min": 16,
+    "fm_4_5_6_n_output_units": 4,
 }
