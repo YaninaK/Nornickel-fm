8adf4b0ed18842c873bc02c4e7341842ae5aa1c8
YaninaK <kutovaiayp@yahoo.com>
2024-12-07 20:34:30 +0300

    Add src\nornickel_fm\features\__init__.py

---
+++ src/nornickel_fm/features/__init__.py
@@ -0,0 +1,12 @@
+CONFIG = {
+    "fm_1_feature_cols": [
+        "Cu_oreth",
+        "Ni_oreth",
+        "Ore_mass",
+        "Mass_1",
+        "Dens_1",
+        "Cu_1C",
+        "Ni_1C",
+    ],
+    "fm_1_target_cols": ["Ni_1C_min", "Ni_1C_max", "Cu_1C_min", "Cu_1C_max"],
+}
